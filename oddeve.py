8a225b09067dbf9e963567a7e6ad0aaca511075a
adhi <SBMPC.Student@SVKMGRP.COM>
2026-01-29 08:28:04 +0530

    oddeve

---
+++ oddeve.py
@@ -0,0 +1,12 @@
+def check():
+    num = int(input("enter a number:"))
+    if num % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+def main():
+    check()
+
+if __name__ == "__main__":
+    main()  
